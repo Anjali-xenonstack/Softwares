d34225f14d1f2857c2d924bb0b0be5c4322178ff
Harpreet Singh Baath <harpreet@xenondigilabs.com>
2018-07-31 01:40:37 +0530

    added books

---
+++ helm-deployments/Data-Science/Jupyter-Notebook/docker/generate-jupyter-hashed-password.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+jpassword="$JUPYTERPWD"
+salt=$(openssl rand -hex 6)
+hashed_passw=$(echo -n "$jpassword$salt" | openssl dgst -sha1 | sed 's/^.* //')
+jupyter_format_hash="sha1:$salt:$hashed_passw"
+echo $jupyter_format_hash
+
+jupyter notebook --NotebookApp.password=$jupyter_format_hash
